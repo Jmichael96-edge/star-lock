acc08f7f13f82cb9c625b09348bf9562fabe0398
Jeffrey VanHorn <jeffrey.vanhorn@edgeofs.com>
2021-06-04 17:58:00 -0500

    commenting out and getting ready for prod

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const path = require('path');
+// const path = require('path');
 const router = require('express').Router();
 const htmlRoutes = require('./htmlRoutes');
 const resource = require('./resource');
