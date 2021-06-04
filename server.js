a12fe2f12e136607eb418cab2cf8b4ab36d4841d
Jeffrey VanHorn <jeffrey.vanhorn@edgeofs.com>
2021-06-04 17:59:56 -0500

    removed section that is irrelivent

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static(path.join(__dirname, './app')));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
 
 app.use(routes);
 
